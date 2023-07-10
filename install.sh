59db88d38a6a28e7821fbf62c921115715d8bab8
Erik Rogers <erik@rogers-family.net>
2023-07-09 22:05:28 -0400

    Fix install script for Windows

--- install.sh
+++ install.sh
@@ -2,17 +2,6 @@
 
 REPODIR="$(dirname "$(realpath "$0")")"
 
-# Detect Windows and Copy Windows-Specific configs
-UNAME="$(uname -a)"
-if echo "$UNAME" | grep 'Microsoft'; then
-	WINDOWSUSER="/mnt/c/Windows/system32/WindowsPowerShell/v1.0/powershell.exe '$env:UserName'"
-	WINDOWSHOME="/mnt/c/Users/$WINDOWSUSER"
-
-	# alacritty
-	mkdir -p "$WINDOWSHOME/AppData/alacritty"
-	cp "$REPODIR/wsl/alacritty-wsl.yml" "$WINDOWSHOME/AppData/alacritty/alacritty.yml"
-fi
-
 # Make directories if needed
 mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.ssh"
@@ -24,3 +13,17 @@ cp "$REPODIR/.aliases" "$HOME"
 cp "$REPODIR/.bashrc" "$HOME"
 cp "$REPODIR/.profile" "$HOME"
 cp "$REPODIR/.tmux.conf" "$HOME"
+
+# Detect Windows and Copy Windows-Specific configs
+UNAME="$(uname -a)"
+if echo "$UNAME" | grep 'Microsoft'; then
+	if [ -z "$USERPROFILE" ]; then
+		eval "/mnt/c/Windows/system32/cmd.exe /c setx WSLENV USERPROFILE/up"
+		echo "Restart WSL to reset environment variables and rerun script"
+		exit
+	fi
+
+	# alacritty
+	mkdir -p "$USERPROFILE/AppData/alacritty"
+	cp "$REPODIR/wsl/alacritty-wsl.yml" "$USERPROFILE/AppData/Roaming/alacritty/alacritty.yml"
+fi
