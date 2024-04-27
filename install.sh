57b7ff30c98ca74afc1d98ab1bd8a7296b509e4d
Erik Rogers <erik@rogers-family.net>
2024-04-27 13:59:03 -0400

    Add neomutt mbsyncrc and msmtprc configs

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-REPODIR="$HOME/dotfiles"
-REPOURL="https://github.com/erogers88/dotfiles"
+# Get script directory
+SCRIPTDIR="$(dirname "$(realpath "$0")")"
 
-# Create temp repo
-git clone "$REPOURL" "$REPODIR" || exit
-
-# Remove any existing Neovim configs and state
+# Remove any existing nvim files
 rm -rf "$HOME/.config/nvim"
 rm -rf "$HOME/.local/share/nvim"
 rm -rf "$HOME/.local/state/nvim"
@@ -16,14 +13,21 @@ mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.ssh"
 mkdir -p "$HOME/.npm-global/bin"
 mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/Maildir/personal"
+mkdir -p "$HOME/Maildir/work"
 
 # Copy all config files
-cp -r "$REPODIR/.config/"* "$HOME/.config"
-cp -r "$REPODIR/.ssh/"* "$HOME/.ssh"
-cp "$REPODIR/.aliases" "$HOME"
-cp "$REPODIR/.bashrc" "$HOME"
-cp "$REPODIR/.profile" "$HOME"
-cp "$REPODIR/.tmux.conf" "$HOME"
+cp -r "$SCRIPTDIR/.config/"* "$HOME/.config"
+cp -r "$SCRIPTDIR/.ssh/"* "$HOME/.ssh"
+cp "$SCRIPTDIR/.aliases" "$HOME"
+cp "$SCRIPTDIR/.bashrc" "$HOME"
+cp "$SCRIPTDIR/.profile" "$HOME"
+cp "$SCRIPTDIR/.tmux.conf" "$HOME"
+cp "$SCRIPTDIR/.mbsyncrc" "$HOME"
+cp "$SCRIPTDIR/.msmtprc" "$HOME"
+
+echo "copy ~/.config/neomutt/accounts.<personal | work> to ~/.config/neomutt/accounts"
+echo "create ~/.msmtppass.gpg using gpg to encrypt password"
 
 # Detect Windows and Copy Windows-Specific configs
 UNAME="$(uname -a)"
@@ -36,15 +40,12 @@ if echo "$UNAME" | grep 'Microsoft'; then
 
 	# alacritty
 	mkdir -p "$USERPROFILE/AppData/alacritty"
-	cp "$REPODIR/wsl/alacritty-wsl.yml" "$USERPROFILE/AppData/Roaming/alacritty/alacritty.yml"
+	cp "$SCRIPTDIR/wsl/alacritty-wsl.yml" "$USERPROFILE/AppData/Roaming/alacritty/alacritty.yml"
 
 	# node fix for musl-based system (check for apk program)
 	if [ "$(apk --version >/dev/null 2>&1)" = "0" ]; then
-		cp "$REPODIR/wsl/node" "$HOME/.local/bin"
+		cp "$SCRIPTDIR/wsl/node" "$HOME/.local/bin"
 	fi
 	
 fi
-
-# Remove temp repo
-rm -r "$REPODIR"
 
