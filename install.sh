6aa34bc971f53afde3a961f46bf4567a08c59da8
Erik Rogers <erik@rogers-family.net>
2024-05-31 21:54:31 -0400

    Update install.sh to add .pandoc-meta-sans-serif

--- install.sh
+++ install.sh
@@ -63,6 +63,7 @@ cp "$SCRIPTDIR/.tmux.conf" "$HOME"
 cp "$SCRIPTDIR/.mbsyncrc" "$HOME"
 cp "$SCRIPTDIR/.msmtprc" "$HOME"
 cp "$SCRIPTDIR/.gnupg/gpg.conf" "$HOME/.gnupg/gpg.conf"
+cp "$SCRIPTDIR/.pandoc-meta-sans-serif" "$HOME/.pandoc-meta-sans-serif"
 
 # Detect Windows and Copy Windows-Specific configs
 UNAME="$(uname -a)"
