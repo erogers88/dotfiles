acb8d9e56b45e32dea33637a09b26371e3f4a8e8
Erik Rogers <erik@rogers-family.net>
2024-06-17 13:20:33 -0400

    Fix pandoc link colors, gpg-agent

--- install.sh
+++ install.sh
@@ -63,6 +63,7 @@ cp "$SCRIPTDIR/.tmux.conf" "$HOME"
 cp "$SCRIPTDIR/.mbsyncrc" "$HOME"
 cp "$SCRIPTDIR/.msmtprc" "$HOME"
 cp "$SCRIPTDIR/.gnupg/gpg.conf" "$HOME/.gnupg/gpg.conf"
+cp "$SCRIPTDIR/.gnupg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 cp "$SCRIPTDIR/.pandoc-styling.css" "$HOME/.pandoc-styling.css"
 
 # Detect Windows and Copy Windows-Specific configs
