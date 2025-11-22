92daa942d2fa5f15e2466875c821880106df4adf
Erik Rogers <erik@rogers-family.net>
2025-11-22 16:16:42 -0500

    lsp-mason.lua change setup_handlers to setup

--- .config/nvim/lua/plugins/lsp-mason.lua
+++ .config/nvim/lua/plugins/lsp-mason.lua
@@ -70,14 +70,18 @@ return {
         ensure_installed = servers,
       }
 
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
+      mason_lspconfig.setup {
+        {
+          handlers = {
+            function(server_name)
+              require('lspconfig')[server_name].setup {
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = servers[server_name],
+              }
+            end,
           }
-        end,
+        }
       }
     end
   },
